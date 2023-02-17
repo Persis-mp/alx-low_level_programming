233e3e1028d342878284e7eac67878e368992789
Persis-m <ppm.enyonam19@gmail.com>
2023-02-17 02:29:17 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- writ all integer numbers
+ * starting from zero to 10
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
