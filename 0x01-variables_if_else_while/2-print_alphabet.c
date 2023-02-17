cebf35113f975dbdf593c972fefcb1d089c2ac1d
Persis-m <ppm.enyonam19@gmail.com>
2023-02-17 02:09:58 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- writte the alphabet
+ * return in lowercase
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
