ff9d22c6c3addcee5ecd33a92094b373a0cacffb
Persis-m <ppm.enyonam19@gmail.com>
2023-02-27 04:21:04 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half- prints half of a string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int p = 0;
+
+	while (*str != '\0')
+	{
+		p++;
+		str++;
+	}
+	str -= (p / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
