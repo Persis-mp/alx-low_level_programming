cc03e175e02e9ee5eee402ea7249ec546b2b9069
Persis-m <ppm.enyonam19@gmail.com>
2023-03-01 03:51:10 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper- change lowercase
+ * @s: input string
+ *
+ * Return: the pointer to dest
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) = *(s + i) - 32;
+		 i++;
+	}
+	return (s);
+}
