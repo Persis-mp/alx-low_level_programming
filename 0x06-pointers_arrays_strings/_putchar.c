4d63374d8510e65c7fa5a783ae9f2bf10114445a
Persis-m <ppm.enyonam19@gmail.com>
2023-03-01 03:20:23 -0800

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+es (11 sloc)  184 Bytes
+
+#include <unistd.h>
+
+/**
+ * _putchar- write the character c to stdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
