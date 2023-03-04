208fb185ffaa2e25c7b0d4913dfd2ad31550d977
Persis-m <ppm.enyonam19@gmail.com>
2023-03-04 09:25:20 -0800

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string- set the value of the pointer to char
+ * @s: source address
+ * @to: target address
+ *
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
