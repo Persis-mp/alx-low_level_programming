816f84ca6a8ad771fc5305730bc7d43687a3c670
Persis-m <ppm.enyonam19@gmail.com>
2023-03-04 10:06:47 -0800

    100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-void set_string(char **s, char *to)
+void set_string(char **s, char *to);
 
 #endif
