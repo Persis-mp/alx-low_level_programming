b6cbd089b94bbbb5e6fdd3a88757e549d45b1c15
Persis-m <ppm.enyonam19@gmail.com>
2023-03-08 03:31:36 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * power_operation- natural sqrt root of number
+ * @n: number
+ * @c: iterator
+ *
+ * Return: sqrt or -1
+ */
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+/**
+ * _sqrt_recursion- natural square root of number
+ * @n: number
+ *
+ * Return: number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (power_operation(n, 2));
+}
