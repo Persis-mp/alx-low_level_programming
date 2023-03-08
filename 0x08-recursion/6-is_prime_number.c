33c404698ffaef8808f529fff54566a7462ccef1
Persis-m <ppm.enyonam19@gmail.com>
2023-03-08 03:41:03 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * is_prime- detect prime numner
+ * @n: number
+ * @c: iterator
+ *
+ * Return: 1 if n is prime or 0 if n is not
+ */
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number- prime number
+ * @n: number
+ *
+ * Return: 1 if n is a prime or 0 if not a prime number
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
