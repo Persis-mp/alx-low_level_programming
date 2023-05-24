35dbe75cfd46cbccea14a095814decc3f6cddbac
Persis-mp <Persis-mp>
2023-05-23 19:00:50 -0700

    commit all

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower- check for lower characters
+ * lowercase
+ * @c: the charater
+ *
+ * Return: 1 if letter is lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
