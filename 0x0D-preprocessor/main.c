79a94a10e794c9c581f1e610102df7a1e0769adc
Persis-mp <ppm.enyonam19@gmail.com>
2023-06-19 14:20:25 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "0-object_like_macro.h"
+#include "main.h"
+
+/**
+ * main- print an integer value
+ * Return: 0.
+ */
+
+int main(void)
+{
+	_putchar(SIZE);
+	return (0);
+}
